87fc335c27922bbcd8faaba5004a5ba868cb36a5
itscomputers <its_computers@protonmail.com>
2020-08-08 18:02:10 -0600

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "running ruby tests"
+ruby src/ruby/tests.rb
+
+echo "deploying to github"
+git worktree add /tmp/book gh-pages
+mdbook build
+rm -rf /tmp/book/*
+cp -rp book/* /tmp/book/
+cd /tmp/book
+git add -A
+git commit -m "deployed on $(shell date)"
+git push origin gh-pages
+cd -
+git worktree remove /tmp/book
+
